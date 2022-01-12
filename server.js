61f04b076d98bcf85bace6c7aa86c2199bb34462
unknown <sohaib.najar@gmail.com>
2022-01-12 21:03:22 +0200

    3rd commit

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
-const router = require("./routes");
+// const router = require("./routes");
 
-// express
-const express = require("express");
-const cors = require("cors");
+// // express
+// const express = require("express");
+// const cors = require("cors");
 
-// app config
-const app = express();
-app.use(express.json());
-app.use(cors());
+// // app config
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
 
-const PORT = 3333;
+// const PORT = 3333;
 
-// API router
+// // API router
 // app.use("/", router);
-app.listen(process.env.PORT || PORT, () =>
-  console.log(`connected on Port ${PORT}`)
-);
+// app.listen(process.env.PORT || PORT, () =>
+//   console.log(`connected on Port ${PORT}`)
+// );
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
